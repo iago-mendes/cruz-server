9a27e620210a4555f9fbd2c090862194f0780d63
Iago <iagobrazmendes@gmail.com>
2020-08-31 11:50:50 -0300

    create first route

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,16 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+import routes from './routes'
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-mongoose.connect('mongodb://localhost:27017/cruz', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/cruz', {useNewUrlParser: true, useUnifiedTopology: true})
+
+app.use(routes)
 
 app.listen(1973, () => console.log('server is running'))
