2c40afb914593b4a4257f320d3fac648926d74d4
Iago <iagobrazmendes@gmail.com>
2020-08-31 11:41:37 -0300

    connect with db

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+mongoose.connect('mongodb://localhost:27017/cruz', {useNewUrlParser: true})
 
 app.listen(1973, () => console.log('server is running'))
