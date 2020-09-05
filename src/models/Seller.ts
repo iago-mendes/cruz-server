3ed9d456e84f5cf124dc9766a60146da0bc8341f
Iago <iagobrazmendes@gmail.com>
2020-09-05 20:16:24 -0300

    add Seller model

---
+++ src/models/Seller.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const SellerSchema = new mongoose.Schema(
+{
+    nome: {type: String, required: true},
+    imagem: {type: String, required: false},
+    email: {type: String, required: true},
+    senha: {type: String, required: true},
+    admin: {type: Boolean, required: true},
+    telefones:
+    [{
+        numero: {type: Number, required: true},
+        whatsapp: {type: Boolean, required: true}
+    }],
+    representadas:
+    [{
+        id: {type: mongoose.Schema.Types.ObjectId, ref: 'Vendedor', required: true},
+        comissao: {type: Number, required: true}
+    }]
+})
+
+mongoose.model('Vendedor', SellerSchema)
