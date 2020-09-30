e7672935603ef9a6f8b785bebae0d0ad0720de79
Iago <iagobrazmendes@gmail.com>
2020-09-30 11:53:47 -0300

    reduce and filter info in 'list clients'

--- src/controllers/ClientController.ts
+++ src/controllers/ClientController.ts
@@ -2,6 +2,15 @@ import { Request, Response, NextFunction } from 'express'
 
 import Client from '../models/Client'
 
+interface List
+{
+    id: string
+    imagem: string
+    nome_fantasia: string
+    razao_social: string
+    status: {ativo: boolean, aberto: boolean, nome_sujo: boolean}
+}
+
 export default class ClientController
 {
     async create(req: Request, res: Response, next: NextFunction)
@@ -41,8 +50,43 @@ export default class ClientController
     async list(req: Request, res: Response, next: NextFunction)
     {
         try {
+            const {cidade} = req.query
+            console.log(String(cidade))
+
+            let list: List[] = []
             const clients = await Client.find()
-            return res.json(clients)
+            
+            const promises = clients.map(client =>
+            {
+                if (cidade !== undefined && cidade !== '')
+                {
+                    if (client.endereco.cidade === cidade)
+                    {
+                        list.push(
+                        {
+                            id: client.id,
+                            imagem: String(client.imagem),
+                            nome_fantasia: client.nome_fantasia,
+                            razao_social: client.razao_social,
+                            status: client.status
+                        })
+                    }
+                }
+                else
+                {
+                    list.push(
+                    {
+                        id: client.id,
+                        imagem: String(client.imagem),
+                        nome_fantasia: client.nome_fantasia,
+                        razao_social: client.razao_social,
+                        status: client.status
+                    })
+                }
+            })
+            await Promise.all(promises)
+
+            return res.json(list)
         } catch (error) {
             next(error)
         }
