83d7e5dce82396dce8e307ba98db67e7018df02e
Iago <iagobrazmendes@gmail.com>
2020-10-02 09:55:11 -0300

    add upload middleware to routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,10 @@
 import express from 'express'
+import multer from 'multer'
 
 const routes = express.Router()
+
+import multerConfig from './config/multer'
+const upload = multer(multerConfig)
 
 import CompanyController from './controllers/CompanyController'
 import SellerController from './controllers/SellerController'
@@ -12,22 +16,22 @@ const Seller = new SellerController()
 const Client = new ClientController()
 const Request = new RequestController()
 
-routes.post('/companies', Company.create)
-routes.put('/companies/:id', Company.update)
+routes.post('/companies', upload.single('image'), Company.create)
+routes.put('/companies/:id', upload.single('image'), Company.update)
 routes.delete('/companies/:id', Company.remove)
 routes.get('/companies', Company.list)
 routes.get('/companies/:id', Company.show)
 routes.get('/companies-all', Company.all)
 
-routes.post('/sellers', Seller.create)
-routes.put('/sellers/:id', Seller.update)
+routes.post('/sellers', upload.single('image'), Seller.create)
+routes.put('/sellers/:id', upload.single('image'), Seller.update)
 routes.delete('/sellers/:id', Seller.remove)
 routes.get('/sellers', Seller.list)
 routes.get('/sellers/:id', Seller.show)
 routes.get('/sellers-all', Seller.all)
 
-routes.post('/clients', Client.create)
-routes.put('/clients/:id', Client.update)
+routes.post('/clients', upload.single('image'), Client.create)
+routes.put('/clients/:id', upload.single('image'), Client.update)
 routes.delete('/clients/:id', Client.remove)
 routes.get('/clients', Client.list)
 routes.get('/clients/:id', Client.show)
