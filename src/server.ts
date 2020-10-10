bae294797037162fec7810e2cbbfb4d10b05664e
Iago <iagobrazmendes@gmail.com>
2020-10-10 08:46:39 -0300

    organize db connection

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 
 import routes from './routes'
-import { url } from './credentials'
+import { dbName, password } from './config/db'
 
 const app = express()
 
@@ -11,7 +11,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+mongoose.connect(
+    `mongodb+srv://admin:${password}@cluster0.c7b5n.mongodb.net/${dbName}?retryWrites=true&w=majority`,
+    {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}
+)
 mongoose.connection
 .once('open', () => console.log('connection has been made'))
 .on('error', error => console.log('[connection error]: ', error))
