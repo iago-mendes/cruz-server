4592229ee6912ed96f877d96a14af23a1505c5c3
Iago <iagobrazmendes@gmail.com>
2020-11-02 10:39:44 -0300

    change login response

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -10,14 +10,17 @@ export default
 {
     logIn: async (req: Request, res: Response) =>
     {
-        let user = {email: '', password: '', id: ''}
+        let user = {email: '', password: '', id: '', role: ''}
 
         const client = await Client.findOne({email: req.body.email})
-        if (client) user = {email: client.email, password: client.senha, id: client._id}
+        if (client) user = {email: client.email, password: client.senha, id: client._id, role: 'client'}
         else
         {
             const seller = await Seller.findOne({email: req.body.email})
-            if (seller) user = {email: seller.email, password: seller.senha, id: seller._id}
+            if (seller) user =
+            {
+                email: seller.email, password: seller.senha, id: seller._id, role: seller.admin ? 'admin' : 'seller'
+            }
             else return res.status(404).send({ message: "Usuário não encontrado." })
         }
 
@@ -29,6 +32,6 @@ export default
         })
 
         const token = jwt.sign({id: user.email}, secret)
-        return res.status(200).send({email: user.email, id: user.id, token})
+        return res.status(200).send({token, id: user.id, role: user.role})
     }
 }
