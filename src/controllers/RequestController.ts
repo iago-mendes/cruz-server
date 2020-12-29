020203a90ffbcb9e92e5c8e98652a9a8d10d1637
Iago <iagobrazmendes@gmail.com>
2020-12-29 09:09:57 -0300

    organize code

--- src/controllers/RequestController.ts
+++ src/controllers/RequestController.ts
@@ -7,123 +7,123 @@ import Seller from '../models/Seller'
 
 interface ListInterface
 {
-		id: string
-		data: Date
-		cliente: string
-		vendedor: string
-		representada: string
-		tipo: {venda: boolean, troca: boolean}
-		status: {concluido: boolean, enviado: boolean, faturado: boolean}
+	id: string
+	data: Date
+	cliente: string
+	vendedor: string
+	representada: string
+	tipo: {venda: boolean, troca: boolean}
+	status: {concluido: boolean, enviado: boolean, faturado: boolean}
 }
 
 interface Product
 {
-		id: string
-		nome: string
-		imagem: string
-		quantidade: number
-		preco: number
-		precoTabela: number
-		ipi: number
-		st: number
-		subtotal: number
+	id: string
+	nome: string
+	imagem: string
+	quantidade: number
+	preco: number
+	precoTabela: number
+	ipi: number
+	st: number
+	subtotal: number
 }
 
 interface ShowInterface
 {
-		id: string
-		data: Date
-		condicao: string
-		digitado_por?: string
-		peso?: number
-		tipo: {venda: boolean, troca: boolean}
-		status: {concluido: boolean, enviado: boolean, faturado: boolean}
-		cliente: {id: string, nome: string}
-		vendedor: {id: string, nome: string}
-		representada: {id: string, nome: string}
-		linha: {id: string, nome: string}
-		produtos: Array<Product>
-		descontoTotal: number
-		valorTotalProdutos: number
-		valorTotal: number
+	id: string
+	data: Date
+	condicao: string
+	digitado_por?: string
+	peso?: number
+	tipo: {venda: boolean, troca: boolean}
+	status: {concluido: boolean, enviado: boolean, faturado: boolean}
+	cliente: {id: string, nome: string}
+	vendedor: {id: string, nome: string}
+	representada: {id: string, nome: string}
+	linha: {id: string, nome: string}
+	produtos: Array<Product>
+	descontoTotal: number
+	valorTotalProdutos: number
+	valorTotal: number
 }
 
 const defaultList =
 {
-		id: '',
-		data: new Date(),
-		cliente: '',
-		vendedor: '',
-		representada: '',
-		tipo: {venda: false, troca: false},
-		status: {concluido: false, enviado: false, faturado: false}
+	id: '',
+	data: new Date(),
+	cliente: '',
+	vendedor: '',
+	representada: '',
+	tipo: {venda: false, troca: false},
+	status: {concluido: false, enviado: false, faturado: false}
 }
 
 export default class RequestController
 {
 	async create(req: Request, res: Response, next: NextFunction)
 	{
-			try {
-					const request = req.body
-					await RequestModel.create(request)
-					return res.status(201).send()
-			} catch (error) {
-					next(error)
-			}
+		try {
+			const request = req.body
+			await RequestModel.create(request)
+			return res.status(201).send()
+		} catch (error) {
+			next(error)
+		}
 	}
 
 	async update(req: Request, res: Response, next: NextFunction)
 	{
-			try {
-					const request = req.body
-					const tmp = await RequestModel.findByIdAndUpdate(req.params.id, request, {new: true})
-					res.status(200).send()
-					return tmp
-			} catch (error) {
-					next(error)
-			}
+		try {
+			const request = req.body
+			const tmp = await RequestModel.findByIdAndUpdate(req.params.id, request, {new: true})
+			res.status(200).send()
+			return tmp
+		} catch (error) {
+			next(error)
+		}
 	}
 
 	async remove(req: Request, res: Response, next: NextFunction)
 	{
-			try {
-					const tmp = await RequestModel.findByIdAndDelete(req.params.id)
-					res.status(200).send()
-					return tmp
-			} catch (error) {
-					next(error)
-			}
+		try {
+			const tmp = await RequestModel.findByIdAndDelete(req.params.id)
+			res.status(200).send()
+			return tmp
+		} catch (error) {
+			next(error)
+		}
 	}
 
 	async list(req: Request, res: Response, next: NextFunction)
 	{
-			try {
-					let list: ListInterface[] = []
-					const requests = await RequestModel.find()
+		try {
+			let list: ListInterface[] = []
+			const requests = await RequestModel.find()
 
-					const promises = requests.map(async request =>
-					{
-							const client = await Client.findById(request.cliente)
-							const seller = await Seller.findById(request.vendedor)
-							const company = await Company.findById(request.representada)
-							const tmp =
-							{
-									id: request._id,
-									data: request.data,
-									cliente: String(client?.nome_fantasia),
-									vendedor: String(seller?.nome),
-									representada: String(company?.nome_fantasia),
-									tipo: request.tipo,
-									status: request.status
-							}
-							list.push(tmp)
-					})
-					await Promise.all(promises)
+			const promises = requests.map(async request =>
+			{
+				const client = await Client.findById(request.cliente)
+				const seller = await Seller.findById(request.vendedor)
+				const company = await Company.findById(request.representada)
+				const tmp =
+				{
+					id: request._id,
+					data: request.data,
+					cliente: String(client?.nome_fantasia),
+					vendedor: String(seller?.nome),
+					representada: String(company?.nome_fantasia),
+					tipo: request.tipo,
+					status: request.status
+				}
+				list.push(tmp)
+			})
+			await Promise.all(promises)
 
-					return res.json(list)
-			} catch (error) {
-					next(error)
-			}
+			return res.json(list)
+		} catch (error) {
+			next(error)
+		}
 	}
 
 	async show(req: Request, res: Response, next: NextFunction)
--- src/models/Request.ts
+++ src/models/Request.ts
@@ -2,46 +2,46 @@ import mongoose from 'mongoose'
 
 type RequestType = mongoose.Document &
 {
-    data: Date
-    condicao: string
-    digitado_por?: string
-    cliente: string
-    vendedor: string
-    representada: string
-    linha: string
-    peso?: number
-    tipo: {venda: boolean, troca: boolean}
-    status: {concluido: boolean, enviado: boolean, faturado: boolean}
-    produtos: Array<{id: string, quantidade: number, preco: number}>
+	data: Date
+	condicao: string
+	digitado_por?: string
+	cliente: string
+	vendedor: string
+	representada: string
+	linha: string
+	peso?: number
+	tipo: {venda: boolean, troca: boolean}
+	status: {concluido: boolean, enviado: boolean, faturado: boolean}
+	produtos: Array<{id: string, quantidade: number, preco: number}>
 }
 
 const RequestSchema = new mongoose.Schema(
 {
-    data: {type: Date, default: Date.now(), required: true},
-    condicao: {type: String, required: true},
-    digitado_por: {type: String, required: false},
-    cliente: {type: mongoose.Schema.Types.ObjectId, ref: 'Cliente', required: true},
-    vendedor: {type: mongoose.Schema.Types.ObjectId, ref: 'Vendedor', required: true},
-    representada: {type: mongoose.Schema.Types.ObjectId, ref: 'Representada', required: true},
-    linha: {type: mongoose.Schema.Types.ObjectId, ref: 'Representada.linhas', required: true},
-    peso: {type: Number, required: false},
-    tipo:
-    {
-        venda: {type: Boolean, required: true},
-        troca: {type: Boolean, required: true}
-    },
-    status:
-    {
-        concluido: {type: Boolean, required: true},
-        enviado: {type: Boolean, required: true},
-        faturado: {type: Boolean, required: true}
-    },
-    produtos:
-    [{
-        id: {type: mongoose.Schema.Types.ObjectId, ref: 'Representada.linhas.produtos', required: true},
-        quantidade: {type: Number, required: true},
-        preco: {type: Number, required: true}
-    }]
+	data: {type: Date, default: Date.now(), required: true},
+	condicao: {type: String, required: true},
+	digitado_por: {type: String, required: false},
+	cliente: {type: mongoose.Schema.Types.ObjectId, ref: 'Cliente', required: true},
+	vendedor: {type: mongoose.Schema.Types.ObjectId, ref: 'Vendedor', required: true},
+	representada: {type: mongoose.Schema.Types.ObjectId, ref: 'Representada', required: true},
+	linha: {type: mongoose.Schema.Types.ObjectId, ref: 'Representada.linhas', required: true},
+	peso: {type: Number, required: false},
+	tipo:
+	{
+		venda: {type: Boolean, required: true},
+		troca: {type: Boolean, required: true}
+	},
+	status:
+	{
+		concluido: {type: Boolean, required: true},
+		enviado: {type: Boolean, required: true},
+		faturado: {type: Boolean, required: true}
+	},
+	produtos:
+	[{
+		id: {type: mongoose.Schema.Types.ObjectId, ref: 'Representada.linhas.produtos', required: true},
+		quantidade: {type: Number, required: true},
+		preco: {type: Number, required: true}
+	}]
 })
 
 export default mongoose.model<RequestType>('Pedido', RequestSchema)
