a53f214f680631b964160852e9ea3142b40f6396
Iago <iagobrazmendes@gmail.com>
2021-03-01 10:58:13 -0300

    try to fix mail bug [failed]

--- src/services/gmail/index.ts
+++ src/services/gmail/index.ts
@@ -10,23 +10,23 @@ const validFrom =
 
 export function send(subject: string, text: string, to: string, from: string = validFrom[0])
 {
-  const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString('base64')}?=`
-  const messageParts =
+	const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString('base64')}?=`
+	const messageParts =
 	[
-    `From: Cruz Representações <${from}>`,
-    `To: ${to}`,
-    'Content-Type: text/html charset=utf-8',
-    'MIME-Version: 1.0',
-    `Subject: ${utf8Subject}`,
-    '',
-    text,
-  ]
-  const message = messageParts.join('\n')
+		`From: Cruz Representacoes <${from}>`,
+		`To: ${to}`,
+		'Content-Type: text/html charset=utf-8',
+		'MIME-Version: 1.0',
+		`Subject: ${utf8Subject}`,
+		'',
+		text,
+	]
+	const message = messageParts.join('\n')
 	const encodedMessage = Buffer.from(message)
-    .toString('base64')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=+$/, '')
+		.toString('base64')
+		.replace(/\+/g, '-')
+		.replace(/\//g, '_')
+		.replace(/=+$/, '')
 
 	async function callback(auth: any)
 	{
