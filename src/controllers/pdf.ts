50090ba2a1f81583bb36e2e83949df9228ff26e8
Iago <iagobrazmendes@gmail.com>
2021-03-06 14:24:28 -0300

    start handleing pdf request

--- src/controllers/pdf.ts
+++ src/controllers/pdf.ts
@@ -5,12 +5,25 @@ import {createPdf} from '../services/pdf'
 
 const pdf =
 {
+	request: async (req: Request, res: Response) =>
+	{
+		const {requestId} = req.params
+
+		const request = await RequestModel.findById(requestId)
+		if (!request)
+			return res.status(404).json({message: 'Pedido não encontrado!'})
+		
+		const content = `Id do cliente: ${request.cliente}`
+
+		const pdf = await createPdf(content)
+		return res.contentType('application/pdf').send(pdf)
+	},
+
 	general: async (req: Request, res: Response) =>
 	{
 		const {content} = req.body
 
 		const pdf = await createPdf(content)
-		
 		return res.contentType('application/pdf').send(pdf)
 	}
 }
