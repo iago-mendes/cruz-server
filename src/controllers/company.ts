f76fc683dcbf47acc01903d8e1f176050a766626
Iago <iagobrazmendes@gmail.com>
2021-03-11 10:08:44 -0300

    handle changes in company controller

--- src/controllers/company.ts
+++ src/controllers/company.ts
@@ -1,4 +1,4 @@
-import {Request, Response, NextFunction} from 'express'
+import {Request, Response} from 'express'
 import fs from 'fs'
 import path from 'path'
 
@@ -13,9 +13,9 @@ interface ListInterface
 	descricao_curta: string
 }
 
-export default
+const company =
 {
-	async create(req: Request, res: Response, next: NextFunction)
+	create: async (req: Request, res: Response) =>
 	{
 		const {
 			razao_social, 
@@ -47,176 +47,154 @@ export default
 			site,
 			tabelas,
 			condicoes,
-			linhas: []
+			produtos: []
 		})
 		return res.status(201).send()
 	},
 		
-	async update(req: Request, res: Response, next: NextFunction)
+	update: async (req: Request, res: Response) =>
 	{
-		try {
-			const id = req.params.id
-			const {
-				razao_social, 
-				nome_fantasia,
-				cnpj,
-				telefones,
-				email,
-				comissao,
-				descricao_curta,
-				descricao,
-				site,
-				tabelas,
-				condicoes
-			} = req.body
-			
-			let image = req.file
-			
-			interface Update
-			{
-				[letter: string]: any
-			}
-			let company: Update = {}
+		const id = req.params.id
+		const {
+			razao_social, 
+			nome_fantasia,
+			cnpj,
+			telefones,
+			email,
+			comissao,
+			descricao_curta,
+			descricao,
+			site,
+			tabelas,
+			condicoes
+		} = req.body
+		
+		let image = req.file
+		
+		interface Update
+		{
+			[letter: string]: any
+		}
+		let company: Update = {}
 
-			const previous = await Company.findById(id)
-			if (!previous)
-				return res.json({message: 'company not found'})
-			
-			company['_id'] = id
-			if(image)
+		const previous = await Company.findById(id)
+		if (!previous)
+			return res.json({message: 'company not found'})
+		
+		company['_id'] = id
+		if(image)
+		{
+			company['imagem'] = image.filename
+			if (previous?.imagem)
+				fs.unlinkSync(path.resolve(__dirname, '..', '..', 'uploads', previous.imagem))
+		}
+		if(razao_social)
+			company['razao_social'] = razao_social
+		if(nome_fantasia)
+			company['nome_fantasia'] = nome_fantasia
+		if(cnpj)
+			company['cnpj'] = cnpj
+		if(telefones)
+			company['telefones'] = JSON.parse(telefones)
+		if(email)
+			company['email'] = email
+		if(comissao)
+			company['comissao'] = JSON.parse(comissao)
+		if(descricao_curta)
+			company['descricao_curta'] = descricao_curta
+		if(descricao)
+			company['descricao'] = descricao
+		if(site)
+			company['site'] = site
+		if(tabelas)
+			company['tabelas'] = JSON.parse(tabelas)
+		if(condicoes)
+			company['condicoes'] = JSON.parse(condicoes)
+		
+		const tmp = Company.findByIdAndUpdate(id, company)
+		res.status(200).send()
+		return tmp
+	},
+		
+	remove: async (req: Request, res: Response) =>
+	{
+		const {id} = req.params
+		
+		const company = await Company.findById(id)
+		if (!company)
+		return res.status(404).json({message: 'company not found!'})
+		
+		if(company.imagem)
+		fs.unlinkSync(path.resolve(__dirname, '..', '..', 'uploads', company.imagem))
+		
+		const tmp = Company.findByIdAndDelete(id)
+		res.status(200).send()
+		return tmp
+	},
+		
+	list: async (req: Request, res: Response) =>
+	{
+		let list: ListInterface[] = []
+		const companies = await Company.find()
+		
+		const promises = companies.map(company =>
+		{
+			list.push(
 			{
-				company['imagem'] = image.filename
-				if (previous?.imagem)
-					fs.unlinkSync(path.resolve(__dirname, '..', '..', 'uploads', previous.imagem))
-			}
-			if(razao_social)
-				company['razao_social'] = razao_social
-			if(nome_fantasia)
-				company['nome_fantasia'] = nome_fantasia
-			if(cnpj)
-				company['cnpj'] = cnpj
-			if(telefones)
-				company['telefones'] = JSON.parse(telefones)
-			if(email)
-				company['email'] = email
-			if(comissao)
-				company['comissao'] = JSON.parse(comissao)
-			if(descricao_curta)
-				company['descricao_curta'] = descricao_curta
-			if(descricao)
-				company['descricao'] = descricao
-			if(site)
-				company['site'] = site
-			if(tabelas)
-				company['tabelas'] = JSON.parse(tabelas)
-			if(condicoes)
-				company['condicoes'] = JSON.parse(condicoes)
-			
-			const tmp = Company.findByIdAndUpdate(id, company)
-			res.status(200).send()
-			return tmp
-		} catch (error) {
-			next(error)
-		}
-	},
+				id: company._id,
+				imagem: formatImage(company.imagem),
+				nome_fantasia: company.nome_fantasia,
+				descricao_curta: String(company.descricao_curta)
+			})
+		})
+		await Promise.all(promises)
 		
-	async remove(req: Request, res: Response, next: NextFunction)
-	{
-		try {
-			const {id} = req.params
-			
-			const company = await Company.findById(id)
-			if (!company)
-			return res.status(404).json({message: 'company not found!'})
-			
-			if(company.imagem)
-			fs.unlinkSync(path.resolve(__dirname, '..', '..', 'uploads', company.imagem))
-			
-			const tmp = Company.findByIdAndDelete(id)
-			res.status(200).send()
-			return tmp
-		} catch (error) {
-			next(error)
-		}
-	},
-		
-	async list(req: Request, res: Response, next: NextFunction)
-	{
-		try {
-			let list: ListInterface[] = []
-			const companies = await Company.find()
-			
-			const promises = companies.map(company =>
-				{
-					list.push(
-						{
-							id: company._id,
-							imagem: formatImage(company.imagem),
-							nome_fantasia: company.nome_fantasia,
-							descricao_curta: String(company.descricao_curta)
-						})
-					})
-					await Promise.all(promises)
-					
-					return res.json(list)
-				} catch (error) {
-					next(error)
-				}
+		return res.json(list)
 	},
 				
-	async show(req: Request, res: Response, next: NextFunction)
+	show: async (req: Request, res: Response) =>
 	{
-		try {
-			const company = await Company.findById(req.params.id)
-			
-			if (!company)
-			return res.status(404).json({message: 'company not found!'})
-			
-			return res.json(
-				{
-					id: company._id,
-					imagem: formatImage(company.imagem),
-					nome_fantasia: company.nome_fantasia,
-					descricao: company.descricao,
-					site: company.site
-				})
-			} catch (error) {
-				next(error)
-			}
+		const company = await Company.findById(req.params.id)
+		
+		if (!company)
+		return res.status(404).json({message: 'company not found!'})
+		
+		return res.json(
+		{
+			id: company._id,
+			imagem: formatImage(company.imagem),
+			nome_fantasia: company.nome_fantasia,
+			descricao: company.descricao,
+			site: company.site
+		})
 	},
 					
-	async raw(req: Request, res: Response, next: NextFunction)
+	raw: async (req: Request, res: Response) =>
 	{
-		try {
-			const companies = await Company.find()
+		const companies = await Company.find()
 
-			const raw = companies.map(company =>
-			{
-				let tmpCompany = company
-				tmpCompany.imagem = formatImage(company.imagem)
-				return tmpCompany
-			})
+		const raw = companies.map(company =>
+		{
+			let tmpCompany = company
+			tmpCompany.imagem = formatImage(company.imagem)
+			return tmpCompany
+		})
 
-			return res.json(raw)
-		} catch (error) {
-			next(error)
-		}
+		return res.json(raw)
 	},
 					
-	async rawOne(req: Request, res: Response, next: NextFunction)
+	rawOne: async (req: Request, res: Response) =>
 	{
-		try {
-			const {id} = req.params
+		const {id} = req.params
 
-			let company = await Company.findById(id)
-			if (!company)
-				return res.status(404).json({message: 'Empresa não encontrada!'})
+		let company = await Company.findById(id)
+		if (!company)
+			return res.status(404).json({message: 'Empresa não encontrada!'})
 
-			company.imagem = formatImage(company.imagem)
+		company.imagem = formatImage(company.imagem)
 
-			return res.json(company)
-		} catch (error) {
-			next(error)
-		}
+		return res.json(company)
 	}
 }
+
+export default company
