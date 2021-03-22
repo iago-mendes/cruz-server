6b76e8fd78fcd280d0fefcccec2dfa929aadd6ef
Iago <iagobrazmendes@gmail.com>
2021-03-22 11:57:38 -0300

    handle set products

--- src/controllers/product/sheet.ts
+++ src/controllers/product/sheet.ts
@@ -11,7 +11,7 @@ const productHeader: Array<
 function getFullHeader(company: CompanyType)
 {
 	const tables = company.tabelas
-	let fullHeader = productHeader
+	let fullHeader = [...productHeader]
 
 	tables.map(table =>
 	{
@@ -23,6 +23,15 @@ function getFullHeader(company: CompanyType)
 	})
 
 	return fullHeader
+}
+
+function getFieldName(field: string)
+{
+	const column = productHeader.find(column => column.field === field)
+	if (!column)
+		return ''
+	
+	return column.name
 }
 
 const productSheet =
@@ -48,7 +57,72 @@ const productSheet =
 
 	setProducts: async (req: Request, res: Response) =>
 	{
-		return res.send()
+		const {company: companyId} = req.params
+		const {header, products: sheetProducts}:
+		{
+			header: string[]
+			products: Array<
+			{
+				[fieldName: string]: string | number
+			}>
+		} = req.body
+
+		const company = await Company.findById(companyId)
+		if (!company)
+			return res.status(404).json({message: 'Representada não encontrada!'})
+		
+		const fullHeader = getFullHeader(company)
+		if (header.length !== fullHeader.length)
+			return res.status(400).json({message: 'Planilha inválida! Número de colunas incorreto!'})
+		
+		let isValid = true
+		fullHeader.map(({name}, index) =>
+		{
+			if (header[index] !== name)
+				isValid = false
+		})
+		if (!isValid)
+			return res.status(400).json({message: 'Planilha inválida! Descrição de colunas incorreta!'})
+
+		interface Table
+		{
+			id: string
+			preco: number
+		}
+		let products = company.produtos
+
+		sheetProducts.map(sheetProduct =>
+		{
+			let tables: Table[] = []
+			
+			fullHeader.map(({name: fieldName, field}) =>
+			{
+				if (fieldName.split(' ')[0] === 'Tabela')
+					tables.push(
+					{
+						id: field,
+						preco: Number(sheetProduct[fieldName])
+					})
+			})
+
+			products.push(
+			{
+				imagem: undefined,
+				codigo: Number(sheetProduct[getFieldName('codigo')]),
+				nome: String(sheetProduct[getFieldName('nome')]),
+				comissao: Number(sheetProduct[getFieldName('comissao')]),
+				unidade: String(sheetProduct[getFieldName('unidade')]),
+				peso: Number(sheetProduct[getFieldName('peso')]),
+				volume: Number(sheetProduct[getFieldName('volume')]),
+				ipi: Number(sheetProduct[getFieldName('ipi')]),
+				st: Number(sheetProduct[getFieldName('st')]),
+				tabelas: tables
+			})
+		})
+
+		await Company.findByIdAndUpdate(company._id, {produtos: products})
+
+		return res.status(201).send()
 	}
 }
 
