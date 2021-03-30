192785c849a7ac5602989f76c7c554578e4c4279
Iago <iagobrazmendes@gmail.com>
2021-03-30 09:13:48 -0300

    organize code

--- src/controllers/request.ts
+++ src/controllers/request.ts
@@ -1,4 +1,4 @@
-import {Request, Response, NextFunction, request} from 'express'
+import {Request, Response} from 'express'
 
 import RequestModel from '../models/Request'
 import Company from '../models/Company'
@@ -33,209 +33,187 @@ interface ListInterface
 	status: {concluido: boolean, enviado: boolean, faturado: boolean}
 }
 
-export default
+const request =
 {
-	async create(req: Request, res: Response, next: NextFunction)
+	create: async (req: Request, res: Response) =>
 	{
-		try {
-			const
-			{
-				data,
-				condicao,
-				digitado_por,
-				cliente,
-				vendedor,
-				representada,
-				contato,
-				tipo,
-				status,
-				produtos,
-			} = req.body
+		const
+		{
+			data,
+			condicao,
+			digitado_por,
+			cliente,
+			vendedor,
+			representada,
+			contato,
+			tipo,
+			status,
+			produtos,
+		} = req.body
 
-			const request =
-			{
-				data,
-				condicao,
-				digitado_por,
-				cliente,
-				vendedor,
-				representada,
-				contato,
-				tipo,
-				status,
-				produtos
-			}
-
-			await RequestModel.create(request)
-			return res.status(201).send()
-		} catch (error) {
-			next(error)
+		const request =
+		{
+			data,
+			condicao,
+			digitado_por,
+			cliente,
+			vendedor,
+			representada,
+			contato,
+			tipo,
+			status,
+			produtos
 		}
+
+		await RequestModel.create(request)
+		return res.status(201).send()
 	},
 
-	async update(req: Request, res: Response, next: NextFunction)
+	update: async (req: Request, res: Response) =>
 	{
-		try {
-			const {id} = req.params
-			const
-			{
-				data,
-				condicao,
-				digitado_por,
-				cliente,
-				vendedor,
-				representada,
-				peso,
-				tipo,
-				status,
-				produtos
-			} = req.body
+		const {id} = req.params
+		const
+		{
+			data,
+			condicao,
+			digitado_por,
+			cliente,
+			vendedor,
+			representada,
+			peso,
+			tipo,
+			status,
+			produtos
+		} = req.body
 
-			const request =
-			{
-				data,
-				condicao,
-				digitado_por,
-				cliente,
-				vendedor,
-				representada,
-				peso,
-				tipo,
-				status,
-				produtos
-			}
-
-			const tmp = await RequestModel.findByIdAndUpdate(id, request, {new: true})
-			res.status(200).send()
-			return tmp
-		} catch (error) {
-			next(error)
+		const request =
+		{
+			data,
+			condicao,
+			digitado_por,
+			cliente,
+			vendedor,
+			representada,
+			peso,
+			tipo,
+			status,
+			produtos
 		}
+
+		const tmp = await RequestModel.findByIdAndUpdate(id, request, {new: true})
+		res.status(200).send()
+		return tmp
 	},
 
-	async remove(req: Request, res: Response, next: NextFunction)
+	remove: async (req: Request, res: Response) =>
 	{
-		try {
-			const {id} = req.params
-			const tmp = await RequestModel.findByIdAndDelete(id)
-			res.status(200).send()
-			return tmp
-		} catch (error) {
-			next(error)
-		}
+		const {id} = req.params
+		const tmp = await RequestModel.findByIdAndDelete(id)
+		res.status(200).send()
+		return tmp
 	},
 
-	async list(req: Request, res: Response, next: NextFunction)
+	list: async (req: Request, res: Response) =>
 	{
-		try {
-			let list: ListInterface[] = []
-			const {client: clientId, page: requestedPage} = req.query
+		let list: ListInterface[] = []
+		const {client: clientId, page: requestedPage} = req.query
 
-			const filter = clientId ? {cliente: String(clientId)} : {}
-			const requestsAll = await RequestModel.find(filter)
+		const filter = clientId ? {cliente: String(clientId)} : {}
+		const requestsAll = await RequestModel.find(filter)
 
-			requestsAll.sort((a, b) => a.data < b.data ? 1 : -1)
-			const postsPerPage = 10
-			const totalPages = requestsAll.length !== 0
-				? Math.ceil(requestsAll.length / postsPerPage)
-				: 1
-			res.setHeader('totalPages', totalPages)
+		requestsAll.sort((a, b) => a.data < b.data ? 1 : -1)
+		const postsPerPage = 10
+		const totalPages = requestsAll.length !== 0
+			? Math.ceil(requestsAll.length / postsPerPage)
+			: 1
+		res.setHeader('totalPages', totalPages)
 
-			let page = 1
-			if (requestedPage)
-				page = Number(requestedPage)
+		let page = 1
+		if (requestedPage)
+			page = Number(requestedPage)
 
-			if (!(page > 0 && page <= totalPages))
-				return res.status(400).json({message: 'A página pedida é inválida!'})
-			res.setHeader('page', page)
+		if (!(page > 0 && page <= totalPages))
+			return res.status(400).json({message: 'A página pedida é inválida!'})
+		res.setHeader('page', page)
 
-			const sliceStart = (page - 1) * postsPerPage
-			const requests = requestsAll.slice(sliceStart, sliceStart + postsPerPage)
+		const sliceStart = (page - 1) * postsPerPage
+		const requests = requestsAll.slice(sliceStart, sliceStart + postsPerPage)
 
-			const promises = requests.map(async request =>
+		const promises = requests.map(async request =>
+		{
+			const client = await Client.findById(request.cliente)
+			if (!client)
+				return res.status(404).json({message: 'client not found'})
+
+			const seller = await Seller.findById(request.vendedor)
+			if (request.vendedor && !seller)
+				return res.status(404).json({message: 'seller not found'})
+
+			const company = await Company.findById(request.representada)
+			if (!company)
+				return res.status(404).json({message: 'company not found'})
+
+			const {totalValue} = getPricedProducts(request, company, client)
+
+			const tmp =
 			{
-				const client = await Client.findById(request.cliente)
-				if (!client)
-					return res.status(404).json({message: 'client not found'})
-
-				const seller = await Seller.findById(request.vendedor)
-				if (request.vendedor && !seller)
-					return res.status(404).json({message: 'seller not found'})
-
-				const company = await Company.findById(request.representada)
-				if (!company)
-					return res.status(404).json({message: 'company not found'})
-
-				const {totalValue} = getPricedProducts(request, company, client)
-
-				const tmp =
+				id: request._id,
+				data: request.data,
+				cliente:
 				{
-					id: request._id,
-					data: request.data,
-					cliente:
-					{
-						imagem: formatImage(client.imagem),
-						nome_fantasia: client.nome_fantasia,
-						razao_social: client.razao_social
-					},
-					vendedor:
-					{
-						imagem: formatImage(seller ? seller.imagem : undefined),
-						nome: seller ? seller.nome : 'E-Commerce'
-					},
-					representada:
-					{
-						imagem: formatImage(company.imagem),
-						nome_fantasia: company.nome_fantasia,
-						razao_social: company.razao_social
-					},
-					tipo: request.tipo,
-					status: request.status,
-					valorTotal: totalValue
-				}
-				list.push(tmp)
-			})
-			await Promise.all(promises)
+					imagem: formatImage(client.imagem),
+					nome_fantasia: client.nome_fantasia,
+					razao_social: client.razao_social
+				},
+				vendedor:
+				{
+					imagem: formatImage(seller ? seller.imagem : undefined),
+					nome: seller ? seller.nome : 'E-Commerce'
+				},
+				representada:
+				{
+					imagem: formatImage(company.imagem),
+					nome_fantasia: company.nome_fantasia,
+					razao_social: company.razao_social
+				},
+				tipo: request.tipo,
+				status: request.status,
+				valorTotal: totalValue
+			}
+			list.push(tmp)
+		})
+		await Promise.all(promises)
 
-			return res.json(list)
-		} catch (error) {
-			next(error)
-		}
+		return res.json(list)
 	},
 
-	async show(req: Request, res: Response, next: NextFunction)
+	show: async (req: Request, res: Response) =>
+	{
+	const {id} = req.params
+
+	const {request, error} = await getRequest(id)
+	if (!request)
+		return res.status(404).json({message: error})
+
+	return res.json(request)
+	},
+
+	raw: async (req: Request, res: Response) =>
+	{
+		const requests = await RequestModel.find()
+		return res.json(requests)
+	},
+
+	rawOne: async (req: Request, res: Response) =>
 	{
 		const {id} = req.params
 
-		const {request, error} = await getRequest(id)
+		const request = await RequestModel.findById(id)
 		if (!request)
-			return res.status(404).json({message: error})
+			return res.status(404).json({message: 'request not found'})
 
 		return res.json(request)
-	},
-
-	async raw(req: Request, res: Response, next: NextFunction)
-	{
-		try {
-			const requests = await RequestModel.find()
-			return res.json(requests)
-		} catch (error) {
-			next(error)
-		}
-	},
-
-	async rawOne(req: Request, res: Response, next: NextFunction)
-	{
-		try {
-			const {id} = req.params
-
-			const request = await RequestModel.findById(id)
-			if (!request)
-				return res.status(404).json({message: 'request not found'})
-
-			return res.json(request)
-		} catch (error) {
-			next(error)
-		}
 	}
 }
+
+export default request
