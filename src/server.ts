a60afaad6043c54a62dcb525c4dd9c3739394093
Iago <iagobrazmendes@gmail.com>
2021-04-13 11:15:19 -0300

    increase payload size

--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,8 @@ const app = express()
 dotenv.config()
 
 app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json({limit: '1mb'}))
+app.use(express.urlencoded({extended: true, limit: '1mb'}))
 
 mongoose.connect(
     `mongodb://localhost:27017/${process.env.DB_NAME}?authSource=admin`,
