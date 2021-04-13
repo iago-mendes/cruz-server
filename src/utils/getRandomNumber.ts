7bc3e1ab902a46f7afed1927082d762f9849b578
Iago <iagobrazmendes@gmail.com>
2021-04-13 10:31:13 -0300

    remove console log

--- src/utils/getRandomNumber.ts
+++ src/utils/getRandomNumber.ts
@@ -1,7 +1,6 @@
 function getRandomNumber(length: number)
 {
 	const randomDecimal = Math.random()
-	console.log('[randomDecimal]', randomDecimal)
 	const randomNumber = Math.floor(randomDecimal * (10 * length))
 
 	return randomNumber
