69daf00155d9fea2e89c7619e9e156ef27636ce2
Iago <iagobrazmendes@gmail.com>
2021-04-13 11:48:21 -0300

    try to fix cors problem

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import errorHandler from './errors/handler'
 const app = express()
 dotenv.config()
 
-app.use(cors())
+app.use(cors({origin: true}))
 app.use(express.json({limit: '1mb'}))
 app.use(express.urlencoded({extended: true, limit: '1mb'}))
 
