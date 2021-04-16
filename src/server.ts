7719101a1f9b830ae16c2fd7e2d49b36011c08e3
Iago <iagobrazmendes@gmail.com>
2021-04-16 10:11:38 -0300

    expose headers

--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,7 @@ import errorHandler from './errors/handler'
 const app = express()
 dotenv.config()
 
-app.use(cors({origin: '*'}))
-app.options('*', cors({origin: '*'}))
+app.use(cors({origin: '*', exposedHeaders: '*'}))
 app.use(express.json({limit: '1mb'}))
 app.use(express.urlencoded({extended: true, limit: '1mb'}))
 
