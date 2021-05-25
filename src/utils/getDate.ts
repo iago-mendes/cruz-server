722e83468f954b5b1e000e506aa642790fcff666
Iago <iagobrazmendes@gmail.com>
2021-05-25 10:33:39 -0300

    update get date function

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -1,10 +1,7 @@
-export function getDate()
+export default function getDate()
 {
-	const dateObj = new Date()
-	const month = ('0' + (dateObj.getMonth() + 1)).slice(-2)
-	const date = ('0' + dateObj.getDate()).slice(-2)
-	const year = dateObj.getFullYear()
-	const formatedDate = year + '-' + month + '-' + date
+	const dateObj = new Date(Date.now())
+	const formatedDate = dateObj.toISOString().replace('T', ' ').replace('Z', '')
 
 	return formatedDate
 }
