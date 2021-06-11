bf042ce69419376935ad67e55b627197e5a65d13
Iago <iagobrazmendes@gmail.com>
2021-06-11 11:17:33 -0300

    organize code

--- src/controllers/seller.ts
+++ src/controllers/seller.ts
@@ -17,183 +17,157 @@ interface List
 	funcao: string | undefined
 }
 
-export default
+const sellerController =
 {
-	create: async (req: Request, res: Response, next: NextFunction) =>
+	create: async (req: Request, res: Response) =>
 	{
-		try {
-			const image = req.file
-			const {_id, nome, telefones, email, senha, funcao, admin, representadas} = req.body
+		const image = req.file
+		const {_id, nome, telefones, email, senha, funcao, admin, representadas} = req.body
 
-			const password = bcrypt.hashSync(senha, 10)
-			if (!password)
-				return res.status(500).json({message: 'Algo de errado aconteceu durante a encriptação da senha!'})
-			
-			await Seller.create(
+		const password = bcrypt.hashSync(senha, 10)
+		if (!password)
+			return res.status(500).json({message: 'Algo de errado aconteceu durante a encriptação da senha!'})
+		
+		await Seller.create(
+		{
+			_id: handleObjectId(_id),
+			nome,
+			imagem: image && image.filename,
+			telefones: JSON.parse(telefones),
+			email,
+			senha: password,
+			funcao,
+			admin,
+			representadas: JSON.parse(representadas),
+			modificadoEm: getDate()
+		})
+		return res.status(201).send()
+	},
+
+	update: async (req: Request, res: Response) =>
+	{
+		const {id} = req.params
+		const image = req.file
+		const {nome, telefones, email, funcao, admin, representadas} = req.body
+
+		interface Update
+		{
+			[letter: string]: any
+		}
+		let seller: Update = {}
+
+		seller['_id'] = id
+		if (nome) seller['nome'] = nome
+		if (image)
+		{
+			seller['imagem'] = image.filename
+			const previous = await Seller.findById(id)
+			if (previous?.imagem)
+				try {
+					fs.unlinkSync(path.resolve(__dirname, '..', '..', 'uploads', previous.imagem))
+				} catch (error) {
+					if (error.code == 'ENOENT') console.log('file not found!')
+				}
+		}
+		if (telefones) seller['telefones'] = JSON.parse(telefones)
+		if (email) seller['email'] = email
+		if (funcao) seller['funcao'] = funcao
+		if (admin) seller['admin'] = admin
+		if (representadas) seller['representadas'] = JSON.parse(representadas)
+
+		seller['modificadoEm'] = getDate()
+
+		const tmp = await Seller.findByIdAndUpdate(id, seller)
+		res.status(200).send()
+		return tmp
+	},
+
+	remove: async (req: Request, res: Response) =>
+	{
+		const {id} = req.params
+
+		const company = await Company.findById(id)
+		if (company?.imagem)
+			fs.unlinkSync(path.resolve(__dirname, '..', '..', 'uploads', company.imagem))
+
+		const tmp = await Seller.findByIdAndDelete(id)
+		res.status(200).send()
+		return tmp
+	},
+
+	list: async (req: Request, res: Response) =>
+	{
+		let list: List[] = []
+		const sellers = await Seller.find()
+
+		const promises = sellers.map(seller =>
+		{
+			list.push(
 			{
-				_id: handleObjectId(_id),
-				nome,
-				imagem: image && image.filename,
-				telefones: JSON.parse(telefones),
-				email,
-				senha: password,
-				funcao,
-				admin,
-				representadas: JSON.parse(representadas),
-				modificadoEm: getDate()
+				id: seller._id,
+				imagem: formatImage(seller.imagem),
+				nome: seller.nome,
+				funcao: seller.funcao
 			})
-			return res.status(201).send()
-		} catch (error) {
-			next(error)
+		})
+		await Promise.all(promises)
+
+		return res.json(list)
+	},
+
+	show: async (req: Request, res: Response) =>
+	{
+		const seller = await Seller.findById(req.params.id)
+		if(seller !== null)
+		{
+			let companies: {id: string, nome_fantasia: string}[] = []
+			const promises = seller.representadas.map(async company =>
+			{
+				const tmpCompany = await Company.findById(company.id)
+				companies.push(
+				{
+					id: company.id,
+					nome_fantasia: String(tmpCompany?.nome_fantasia)
+				})
+			})
+			await Promise.all(promises)
+
+			return res.json(
+			{
+				id: seller._id,
+				imagem: formatImage(seller.imagem),
+				nome: seller.nome,
+				funcao: seller.funcao,
+				telefones: seller.telefones,
+				representadas: companies
+			})
 		}
 	},
 
-	update: async (req: Request, res: Response, next: NextFunction) =>
+	raw: async (req: Request, res: Response) =>
 	{
-		try {
-			const {id} = req.params
-			const image = req.file
-			const {nome, telefones, email, funcao, admin, representadas} = req.body
+		const sellers = await Seller.find()
 
-			interface Update
-			{
-				[letter: string]: any
-			}
-			let seller: Update = {}
-
-			seller['_id'] = id
-			if (nome) seller['nome'] = nome
-			if (image)
-			{
-				seller['imagem'] = image.filename
-				const previous = await Seller.findById(id)
-				if (previous?.imagem)
-					try {
-						fs.unlinkSync(path.resolve(__dirname, '..', '..', 'uploads', previous.imagem))
-					} catch (error) {
-						if (error.code == 'ENOENT') console.log('file not found!')
-					}
-			}
-			if (telefones) seller['telefones'] = JSON.parse(telefones)
-			if (email) seller['email'] = email
-			if (funcao) seller['funcao'] = funcao
-			if (admin) seller['admin'] = admin
-			if (representadas) seller['representadas'] = JSON.parse(representadas)
-
-			seller['modificadoEm'] = getDate()
-
-			const tmp = await Seller.findByIdAndUpdate(id, seller)
-			res.status(200).send()
+		return res.json(sellers.map(seller =>
+		{
+			let tmp = seller
+			tmp.imagem = formatImage(tmp.imagem)
 			return tmp
-		} catch (error) {
-			next(error)
-		}
-	},
-
-	remove: async (req: Request, res: Response, next: NextFunction) =>
-	{
-			try {
-				const {id} = req.params
-
-				const company = await Company.findById(id)
-				if (company?.imagem)
-					fs.unlinkSync(path.resolve(__dirname, '..', '..', 'uploads', company.imagem))
-
-				const tmp = await Seller.findByIdAndDelete(id)
-				res.status(200).send()
-				return tmp
-			} catch (error) {
-					next(error)
-			}
-	},
-
-	list: async (req: Request, res: Response, next: NextFunction) =>
-	{
-			try {
-					let list: List[] = []
-					const sellers = await Seller.find()
-
-					const promises = sellers.map(seller =>
-					{
-							list.push(
-							{
-									id: seller._id,
-									imagem: formatImage(seller.imagem),
-									nome: seller.nome,
-									funcao: seller.funcao
-							})
-					})
-					await Promise.all(promises)
-
-					return res.json(list)
-			} catch (error) {
-					next(error)
-			}
-	},
-
-	show: async (req: Request, res: Response, next: NextFunction) =>
-	{
-			try {
-					const seller = await Seller.findById(req.params.id)
-					if(seller !== null)
-					{
-							let companies: {id: string, nome_fantasia: string}[] = []
-							const promises = seller.representadas.map(async company =>
-							{
-									const tmpCompany = await Company.findById(company.id)
-									companies.push(
-									{
-											id: company.id,
-											nome_fantasia: String(tmpCompany?.nome_fantasia)
-									})
-							})
-							await Promise.all(promises)
-
-							return res.json(
-							{
-									id: seller._id,
-									imagem: formatImage(seller.imagem),
-									nome: seller.nome,
-									funcao: seller.funcao,
-									telefones: seller.telefones,
-									representadas: companies
-							})
-					}
-			} catch (error) {
-					next(error)
-			}
-	},
-
-	raw: async (req: Request, res: Response, next: NextFunction) =>
-	{
-			try {
-					const sellers = await Seller.find()
-
-					return res.json(sellers.map(seller =>
-					{
-						let tmp = seller
-						tmp.imagem = formatImage(tmp.imagem)
-						return tmp
-					}))
-			} catch (error) {
-					next(error)
-			}
+		}))
 	},
 	
-	rawOne: async (req: Request, res: Response, next: NextFunction) =>
+	rawOne: async (req: Request, res: Response) =>
 	{
-		try {
-			const {id} = req.params
-			
-			let seller = await Seller.findById(id)
-			if (!seller)
-				return res.status(404).json({message: 'seller not found!'})
+		const {id} = req.params
+		
+		let seller = await Seller.findById(id)
+		if (!seller)
+			return res.status(404).json({message: 'seller not found!'})
 
-			seller.imagem = formatImage(seller.imagem)
+		seller.imagem = formatImage(seller.imagem)
 
-			return res.json(seller)
-		} catch (error) {
-			
-		}
+		return res.json(seller)
 	}
 }
+
+export default sellerController
