23768e3619c7c26f813da8d49585ab19667f13e5
Iago <iagobrazmendes@gmail.com>
2021-08-19 17:32:08 -0300

    fix: send client name in request e-mail subject

--- src/controllers/mail.ts
+++ src/controllers/mail.ts
@@ -14,7 +14,7 @@ const mail = {
 
 		const pdf = await createRequestPdf(request)
 		sendMail(
-			'Pedido no E-commerce',
+			`E-commerce: ${request.cliente.nome_fantasia}`,
 			text,
 			to,
 			'e-commerce@cruzrepresentacoes.com.br',
@@ -33,7 +33,7 @@ const mail = {
 
 		const pdf = await createRequestPdf(request)
 		sendMail(
-			'Pedido no Sistema',
+			`Pedido: ${request.cliente.nome_fantasia}`,
 			text,
 			to,
 			'sistema@cruzrepresentacoes.com.br',
